cede5e0b53d1b54b5898925b59dbbe4b110e775d
victoria-huang <vctrah@gmail.com>
2018-03-15 18:05:08 -0400

    inheritance practice

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def knowledge
+    @knowledge
+  end
+
+  def learn(knowledge_str)
+    @knowledge << knowledge_str
+  end
 end
