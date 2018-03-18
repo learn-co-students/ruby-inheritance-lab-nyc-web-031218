2b0ce9ecb32e1c8ca6fbab1f3c5b097a06d52fab
Al <lin.alice72@gmail.com>
2018-03-17 21:48:50 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+  attr_reader :first_name, :last_name, :knowledge
+
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(smarts)
+    @knowledge << smarts
+  end
 
 end
